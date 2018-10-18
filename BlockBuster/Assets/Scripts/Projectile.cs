4656639a2aa64f583c18da65e17f39041e69d190
Frederik Engels <frederik.engels24@gmail.com>
2018-10-18 11:12:38 +0200

    start VR/AR introduction

---
+++ BlockBuster/Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public float speed;
+	public int damage;
+    
+	Vector3 shootDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+	{
+		this.transform.Translate(shootDirection * speed, Space.World);
+	}
+
+	public void FireProjectile(Ray shootRay) {
+		this.shootDirection = shootRay.direction;
+		this.transform.position = shootRay.origin;
+		rotateInShootDirection();
+	}
+
+	void rotateInShootDirection() {
+		Vector3 newRotation = Vector3.RotateTowards(transform.forward, shootDirection, 0.01f, 0.0f);
+		transform.rotation = Quaternion.LookRotation(newRotation);
+	}
+
+	void OnCollisionEnter(Collision obj) {
+		Enemy enemy = obj.collider.gameObject.GetComponent<Enemy>();
+
+		if (enemy) {
+			enemy.TakeDamage(this.damage);
+		}
+
+		Destroy(this.gameObject);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
